9ceb2c0f7ff52fe7b7c353f8e8c7365594a20745
Brian Morearty <brian@morearty.org>
2014-02-16 16:33:04 -0800

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sfrailsapp::Application.config.secret_key_base = 'ebc8a7e462e492e63425e792b8507b12fbb14176105b51440021cc5cfa32862aea7aaa77973d5a3c92abe4bb49ebba63c1c3b7164f6e315ff5983f9f7482d00c'
